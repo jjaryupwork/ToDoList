afa05de273db7c7c007fcd732fdc39c1970baf12
jjaryupwork <jjaryupwork@gmail.com>
2017-07-03 18:35:04 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,25 +1,17 @@
 function addListItem(){
 	var text = $("#new-text").val();
-	$("#todolist").append('<li><input type="checkbox" class="done"/>'+text+' <button class="delete">Delete</button></li>');
+	$("#todolist").append('<li><input type="checkbox" class="done"/><label>'+text+' </label><button class="delete">Delete</button></li>');
 	$("#new-text").val('');
 	
 }
 function deleteItem(){
 	$(this).parent().remove();
 }
-function finishItem(){
-	if ( $(this).parent().css('textDecoration') == 'line-through' ){
-		$(this).parent().css('textDecoration', 'none');
-	} else {
-		$(this).parent().css('textDecoration', 'line-through');
-		
-	}
 
-}
 
 $(function(){
 	$("#add").on('click', addListItem);
 	
 	$(document).on('click', '.delete', deleteItem);
-	$(document).on('click', '.done', finishItem);
+	
 });
